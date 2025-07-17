139cd37711db8e930385dbabe91645c0adf5b53e
KnightNight101 <141456321+KnightNight101@users.noreply.github.com>
2025-07-17 16:15:00 +0100

    There are now only two dashboards: AdminDashboard.jsx for admin users and Dashboard.jsx for all other users. Login logic and routing have been updated to direct users to the correct dashboard based on their role. All references to team leader and member dashboards have been removed.

--- Draft_2/ui/src/App.jsx
+++ Draft_2/ui/src/App.jsx
@@ -3,8 +3,6 @@ import Authentication from './pages/Authentication.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import ProjectCreation from './pages/ProjectCreation.jsx';
 import AdminDashboard from './pages/AdminDashboard.jsx';
-import LeaderDashboard from './pages/LeaderDashboard.jsx';
-import MemberDashboard from './pages/MemberDashboard.jsx';
 
 import EmployeeList from './pages/EmployeeList.jsx';
 import AddUser from './pages/AddUser.jsx';
@@ -27,8 +25,6 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/create-project" element={<ProjectCreation />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/leader-dashboard" element={<LeaderDashboard />} />
-        <Route path="/member-dashboard" element={<MemberDashboard />} />
         <Route path="/employee-list" element={<EmployeeList />} />
         <Route path="/add-user" element={<AddUser />} />
       </Routes>
